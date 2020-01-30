3f67033db986c2d86834843d8d8886fd4ae61d49
Dhonatan da Silva <dhonatan_silva@hotmail.com>
2020-01-29 22:48:34 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/com/acme/cursomc/services/CategoriaService.java
+++ src/main/java/com/acme/cursomc/services/CategoriaService.java
@@ -2,8 +2,13 @@ package com.acme.cursomc.services;
 
 import java.util.List;
 
+import javax.print.attribute.standard.PageRanges;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.acme.cursomc.domain.Categoria;
@@ -48,5 +53,10 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll(){
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
