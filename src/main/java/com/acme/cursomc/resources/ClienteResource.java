91cb14329fab88e239ca4770afbdef4fa0e56402
Dhonatan da Silva <dhonatan_silva@hotmail.com>
2020-02-04 23:00:03 -0300

    Validação customizada: email não repetido na inserção de Cliente

--- src/main/java/com/acme/cursomc/resources/ClienteResource.java
+++ src/main/java/com/acme/cursomc/resources/ClienteResource.java
@@ -35,15 +35,6 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
-	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO objDto){
-		Cliente obj = service.fromDTO(objDto);
-		obj = service.insert(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-				.path("/{id}").buildAndExpand(obj.getId()).toUri();
-		return ResponseEntity.created(uri).build();
-	}
-	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id){
 		Cliente obj = service.fromDTO(objDto);
