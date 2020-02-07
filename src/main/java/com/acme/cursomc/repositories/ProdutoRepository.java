531b527907161f3291f57a85a33f4dc6eaed2031
Dhonatan da Silva <dhonatan_silva@hotmail.com>
2020-02-07 20:54:19 -0300

    Subtotal e Total do Pedido

--- src/main/java/com/acme/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/acme/cursomc/repositories/ProdutoRepository.java
@@ -2,12 +2,14 @@ package com.acme.cursomc.repositories;
 
 import java.util.List;
 
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.acme.cursomc.domain.Categoria;
 import com.acme.cursomc.domain.Produto;
@@ -15,6 +17,7 @@ import com.acme.cursomc.domain.Produto;
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 
+	@Transactional(readOnly = true)
 	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
 	Page<Produto> search(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
 	
