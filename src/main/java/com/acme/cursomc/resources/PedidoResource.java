8e9dbdf2efc2e6e7bef8176c2fe98369ba6c363e
Dhonatan da Silva <dhonatan_silva@hotmail.com>
2020-02-12 16:35:06 -0300

    Implementando toString de Pedido

--- src/main/java/com/acme/cursomc/resources/PedidoResource.java
+++ src/main/java/com/acme/cursomc/resources/PedidoResource.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.acme.cursomc.domain.Categoria;
 import com.acme.cursomc.domain.Pedido;
 import com.acme.cursomc.services.PedidoService;
 
--- src/main/java/com/acme/cursomc/services/PedidoService.java
+++ src/main/java/com/acme/cursomc/services/PedidoService.java
@@ -14,7 +14,6 @@ import com.acme.cursomc.domain.enums.EstadoPagamento;
 import com.acme.cursomc.repositories.ItemPedidoRepository;
 import com.acme.cursomc.repositories.PagamentoRepository;
 import com.acme.cursomc.repositories.PedidoRepository;
-import com.acme.cursomc.repositories.ProdutoRepository;
 import com.acme.cursomc.services.exception.ObjectNofFoundException;
 
 @Service
@@ -30,10 +29,13 @@ public class PedidoService {
 	private PagamentoRepository pagamentoRepository;
 	
 	@Autowired
-	private ProdutoRepository produtoRepository;
+	private ProdutoService produtoService;
 	
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private ClienteService clienteService;
 	
 	public Pedido find(Integer id) {
 		Pedido obj = repo.getOne(id);
@@ -48,6 +50,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if(obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -59,11 +62,13 @@ public class PedidoService {
 		
 		for(ItemPedido ip: obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoRepository.findById(ip.getProduto().getId()).get().getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 			
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
 
